f52e8fd9e7f765f53673a568753c438c44955a9f
pheiduck <pheiduck@github.com>
2021-06-20 14:02:33 +0200

    Update Rechner
    Signed-off-by: pheiduck <pheiduck@github.com>

--- Rechner.py
+++ Rechner.py
@@ -1,4 +1,12 @@
 num1 = input("Enter a number: ")
+oper = input("Which Operator do you want to use? ")
 num2 = input("Enter another number: ")
-result = float(num1) + float(num2)
-print(result)
+
+if (oper == "+"):
+    print("result: ", float(num1) + float(num2))
+elif (oper == "-"):
+    print("result: ", float(num1) - float(num2))
+elif (oper == "*"):
+    print("result: ", float(num1) * float(num2))
+elif (oper == "/"):
+    print("result: ", float(num1) / float(num2))
