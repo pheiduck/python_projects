341699007c0e2ddcb24901af8b188449cdf96a1e
168912916 859421311 <pheiduck@github.com>
2021-07-24 18:34:36 +0200

    adjust input for numbers
    Signed-off-by: 168912916 859421311 <pheiduck@github.com>

--- Rechner.py
+++ Rechner.py
@@ -1,6 +1,6 @@
-num1 = input("Enter a number: ")
+num1 = input("Enter first number: ")
 oper = input("Which Operator do you want to use? ")
-num2 = input("Enter another number: ")
+num2 = input("Enter second number: ")
 
 if (oper == "+"):
     print("result: ", float(num1) + float(num2))
